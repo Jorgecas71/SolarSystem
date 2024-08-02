b0f981ab5c75e6945d39ff66c13b65d21ab669d8
JORGE CASTRO GUTIÉRREZ <jorgecas71@MacBook-Air-de-JORGE.local>
2024-08-02 19:10:50 +0200

    init project

---
+++ src/test/java/dev/jorge/SolarSystem/SolarSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jorge.SolarSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
